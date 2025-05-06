a47ecfc88ac728c99e77b26641882ea67d810e3b
Sumit Ranjan <sumit.ranjan@indiamart.com>
2025-05-06 23:00:27 +0530

    App logo, name changed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,4 +67,5 @@ dependencies {
     kapt("androidx.room:room-compiler:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
     implementation("com.google.code.gson:gson:2.10.1")
+    implementation("de.hdodenhof:circleimageview:3.1.0")
 }
--- app/src/main/java/com/example/mymovieapp/ui/HomeFragment.kt
+++ app/src/main/java/com/example/mymovieapp/ui/HomeFragment.kt
@@ -1,11 +1,10 @@
 package com.example.mymovieapp.ui
 
-import android.Manifest
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.annotation.RequiresPermission
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
@@ -49,6 +48,9 @@ class HomeFragment : Fragment() {
         } else {
             homeViewModel.loadTrendingFromCache()
             homeViewModel.loadNowShowingFromCache()
+        }
+        binding.appIcon.setOnClickListener {
+            Toast.makeText(requireContext(), "Welcome to MoviePlex", Toast.LENGTH_LONG).show()
         }
     }
 
